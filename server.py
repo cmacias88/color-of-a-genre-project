f97f27b949a56b1cc14971622e47217c49cf941f
Chloe Macias <chloeamacias@gmail.com>
2022-11-19 16:02:16 -0500

    Added api routes to server.py and NavBar Component to App.jsx.

--- server.py
+++ server.py
@@ -1,12 +1,33 @@
 from flask import Flask, jsonify, render_template
-from model import db, Melon, MelonType, connect_to_db
-from time import sleep
+from model import db, User, Visualization, Playlist, PlaylistTrack, Genre, Track, TrackGenre, TrackVisualization, VisualizationData, connect_to_db
+import crud
+
 
 app = Flask(__name__)
-app.secret_key = 'secret'
+app.secret_key = 'dev'
 
 
 @app.route('/')
 def home():
-
     return render_template('index.html')
+
+
+@app.route('/api/users')
+def get_users_json():
+    """ Return a JSON response with all users. """
+
+    users = crud.get_all_users()
+
+    return jsonify({'users': users})
+
+
+@app.route("/api/genres")
+def get_all_genres_json():
+    """ Return a JSON response with all genres. """
+
+    genres = crud.get_all_genres()
+
+    return jsonify({'genres': genres})
+
+
+
--- static/js/App.jsx
+++ static/js/App.jsx
@@ -1,10 +1,45 @@
 const Link = ReactRouterDOM.Link;
+const Route = ReactRouterDOM.Route;
+
+
+function NavBar (props) {
+    return (
+        <nav>
+            <ReactRouterDOM.Link to="/" className="navbar">
+                <span>  Home  </span>
+            </ReactRouterDOM.Link>
+            <ReactRouterDOM.Link to="/user-profile" className="navbar">
+                <span>  My Profile  </span>
+            </ReactRouterDOM.Link>
+            <ReactRouterDOM.Link to="/playlist-selection" className="navbar">
+                <span>  Playlist Selection  </span>
+            </ReactRouterDOM.Link>
+            <ReactRouterDOM.Link to="/spotify-authorization" className="navbar">
+                <span>  Spotify Authorization </span>
+            </ReactRouterDOM.Link>
+        </nav>
+    )
+}
+
+
+function Homepage(props) {
+    return (
+    <React.Fragment>
+    <h1>Welcome!</h1>
+    
+    <li><Link to="/log-in">Log In</Link></li>
+    <li><Link to="/sign-up">Sign Up</Link></li>
+    </React.Fragment>
+    )
+}
+
 
 function Welcome(props) {
     return (
     <p>Welcome {props.username}!</p>
     )
 }
+
 
 function LogInButton() {
     return (
@@ -13,6 +48,20 @@ function LogInButton() {
         </button>
     )
 }
+
+
+function UserSignUp() {
+    return (
+        <div className="form">
+        <form>
+            <input type="text" name="username"></input>
+            <input type="password" name="password"></input>
+            <input type="submit"></input>
+        </form>
+        </div>
+    )
+}
+
 
 function UserLogin() {
     return (
@@ -30,18 +79,33 @@ function UserLogin() {
 function App() {
     return (
         <ReactRouterDOM.BrowserRouter>
-            <NavBar />
-            <ReactRouterDOM.Route exact path="/">
-              <Homepage />
-            </ReactRouterDOM.Route>
-            <ReactRouterDOM.Route exact path="/signup">
-                <SignUp />
-            </ReactRouterDOM.Route>
-            <ReactRouterDOM.Route exact path="/login">
-                <Login />
-            </ReactRouterDOM.Route>
+            <Navbar />
+                <ReactRouterDOM.Route exact path="/">
+                    <Homepage />
+                    <SearchBar />
+                </ReactRouterDOM.Route>
+                <ReactRouterDOM.Route exact path="/sign-up">
+                    <UserSignUp />
+                </ReactRouterDOM.Route>
+                <ReactRouterDOM.Route exact path="/log-in">
+                    <UserLogin />
+                </ReactRouterDOM.Route>
+                <ReactRouterDOM.Route exact path="/user-profile">
+                    <UserProfile />
+                </ReactRouterDOM.Route>
+                <ReactRouterDOM.Route exact path="/playlist-selection">
+                    <PlaylistSelction />
+                </ReactRouterDOM.Route>
+                <ReactRouterDOM.Route exact path="/visualization-generator">
+                    <VisualizationGenerator />
+                </ReactRouterDOM.Route>
+                <ReactRouterDOM.Route exact path="/spotify-authorization">
+                    <SpotifyAuth />
+                </ReactRouterDOM.Route>
         </ReactRouterDOM.BrowserRouter>
-      );
+
+    );
 }
+
 
 ReactDOM.render(<App />, document.querySelector('#root'));
